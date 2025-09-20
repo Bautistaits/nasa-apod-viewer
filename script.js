dd2b34022b590036061134f6d2c09f826c608f42
bauti <136343970+Bautistaits@users.noreply.github.com>
2025-09-19 23:18:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ let titulo = document.querySelector('h1');
 
 boton.onclick = function () {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+        .then(res => res.json())
         .then(fotoDelDia => {
             imagenDelDia.src = fotoDelDia.hdurl;
             titulo.textContent = fotoDelDia.title;
         });
 };
+
 
 let imagenFecha = document.querySelector('#imagenFecha');
 let botonFecha = document.querySelector('#botonFecha');
