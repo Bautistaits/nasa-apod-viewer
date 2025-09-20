40f880248d2b0b9f77df48b17cf1fd1878a7df10
bauti <136343970+Bautistaits@users.noreply.github.com>
2025-09-19 22:52:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-let key = 'kI83SWOY6Bf7Nf9HJdH1CE1Fl6g1m4EsKeU6srDT';
+let key = 'kI83SWOY6Bf7Nf9HJdH1CE1Fl6g1m4EsKeU6srDT'
 
-let imagenDelDia = document.querySelector('#imagenDelDia');
-let boton = document.querySelector('#boton');
-let titulo = document.querySelector('h1');
+let imagenDelDia = document.querySelector('#imagenDelDia')
+let boton = document.querySelector('#boton')
+let titulo = document.querySelector('h1')
 
 boton.onclick = function () {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+    fetch(https:
         .then(res => res.json())
         .then(fotoDelDia => {
             imagenDelDia.src = fotoDelDia.hdurl
             titulo.textContent = fotoDelDia.title
         })
 }
+
 let imagenFecha = document.querySelector('#imagenFecha')
 let botonFecha = document.querySelector('#botonFecha')
 let fechaUsuario = document.querySelector('#fecha')
 
 botonFecha.onclick = function () {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fechaUsuario.value}`)
+    fetch(https:
         .then(res => res.json())
         .then(fotoFecha => {
             imagenFecha.src = fotoFecha.hdurl
         })
 }
+
 let contenedorImagen = document.querySelector('.contenedor-imagen')
 let botonCantidad = document.querySelector('#botonCantidad')
 let cantidadUsuario = document.querySelector('#cantidad')
 
 botonCantidad.onclick = function () {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&count=${cantidadUsuario.value}`)
+    fetch(https:
         .then(res => res.json())
         .then(fotos => {
             contenedorImagen.innerHTML = ''
@@ -41,21 +43,12 @@ botonCantidad.onclick = function () {
         })
 }
 
+
 fechaUsuario.min = '1995-06-16'
 
-
-let hoy = new Date();
+let hoy = new Date()
 let dia = String(hoy.getDate()).padStart(2, '0')
 let mes = String(hoy.getMonth() + 1).padStart(2, '0')
+let anio = hoy.getFullYear()
 
-fechaUsuario.max = `${anio}-${mes}-${dia}`
-
-
-botonFecha.onclick = function () {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=' + key + '&date=' + fechaUsuario.value)
-        .then(function(res) { return res.json(); })
-        .then(function(fotoFecha) {
-            imagenFecha.src = fotoFecha.hdurl;
-        })
-}
-
+fechaUsuario.max = '${anio}-${mes}-${dia}'
