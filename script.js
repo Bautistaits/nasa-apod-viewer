8506910ce03907e820c7b5480ad7355a8ac6afc3
bauti <136343970+Bautistaits@users.noreply.github.com>
2025-09-19 22:55:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-let key = 'kI83SWOY6Bf7Nf9HJdH1CE1Fl6g1m4EsKeU6srDT'
+let key = 'kI83SWOY6Bf7Nf9HJdH1CE1Fl6g1m4EsKeU6srDT';
 
-let imagenDelDia = document.querySelector('#imagenDelDia')
-let boton = document.querySelector('#boton')
-let titulo = document.querySelector('h1')
+let imagenDelDia = document.querySelector('#imagenDelDia');
+let boton = document.querySelector('#boton');
+let titulo = document.querySelector('h1');
 
 boton.onclick = function () {
     fetch(https:
         .then(res => res.json())
         .then(fotoDelDia => {
-            imagenDelDia.src = fotoDelDia.hdurl
-            titulo.textContent = fotoDelDia.title
-        })
-}
+            imagenDelDia.src = fotoDelDia.hdurl;
+            titulo.textContent = fotoDelDia.title;
+        });
+};
 
-let imagenFecha = document.querySelector('#imagenFecha')
-let botonFecha = document.querySelector('#botonFecha')
-let fechaUsuario = document.querySelector('#fecha')
+let imagenFecha = document.querySelector('#imagenFecha');
+let botonFecha = document.querySelector('#botonFecha');
+let fechaUsuario = document.querySelector('#fecha');
 
 botonFecha.onclick = function () {
     fetch(https:
         .then(res => res.json())
         .then(fotoFecha => {
-            imagenFecha.src = fotoFecha.hdurl
-        })
-}
+            imagenFecha.src = fotoFecha.hdurl;
+        });
+};
 
-let contenedorImagen = document.querySelector('.contenedor-imagen')
-let botonCantidad = document.querySelector('#botonCantidad')
-let cantidadUsuario = document.querySelector('#cantidad')
+let contenedorImagen = document.querySelector('.contenedor-imagen');
+let botonCantidad = document.querySelector('#botonCantidad');
+let cantidadUsuario = document.querySelector('#cantidad');
 
 botonCantidad.onclick = function () {
     fetch(https:
         .then(res => res.json())
         .then(fotos => {
-            contenedorImagen.innerHTML = ''
+            contenedorImagen.innerHTML = '';
 
             for (let i = 0; i < fotos.length; i++) {
                 contenedorImagen.innerHTML += `
                     <img src="${fotos[i].url}" alt="${fotos[i].title}">
-                `
+                `;
             }
-        })
-}
+        });
+};
 
 
-fechaUsuario.min = '1995-06-16'
+fechaUsuario.min = '1995-06-16';
 
-let hoy = new Date()
-let dia = String(hoy.getDate()).padStart(2, '0')
-let mes = String(hoy.getMonth() + 1).padStart(2, '0')
-let anio = hoy.getFullYear()
+let hoy = new Date();
+let dia = String(hoy.getDate()).padStart(2, '0');
+let mes = String(hoy.getMonth() + 1).padStart(2, '0');
+let anio = hoy.getFullYear(); 
 
-fechaUsuario.max = '${anio}-${mes}-${dia}'
+fechaUsuario.max = ${anio}-${mes}-${dia};
